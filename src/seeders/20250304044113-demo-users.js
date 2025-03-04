b21bb200eec13136c2c464fc37e7d60ba81f1f27
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-03 22:46:11 -0600

    add: new seeder for crete users :sparkles:

---
+++ src/seeders/20250304044113-demo-users.js
@@ -0,0 +1,45 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("users", [
+      {
+        id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        name: "John Doe",
+        email: "john@gmail.com",
+        password: await bcrypt.hash("123456", 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: "f7c1f1ee-6c54-4b01-90e6-d701748f0851",
+        name: "Jane Doe",
+        email: "jane@gmail.com",
+        password: await bcrypt.hash("123456", 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
