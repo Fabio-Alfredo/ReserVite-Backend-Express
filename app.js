ceeee0fd58a2a8aafa23819eb1ac0a410c47a091
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-04 22:57:34 -0600

    add(auth.controller): new controller for register new user and login user :sparkles:

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./src/middlewares/errorHandler.middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
 
 module.exports = app;
---
+++ src/controller/auth.controller.js
@@ -0,0 +1,65 @@
+const createHttpError = require("http-errors");
+const ErrorCodes = require("../utils/errors/error.codes");
+const authService = require("../services/auth.service");
+const responseHandler = require("../helpers/responsehandler.helper");
+
+/**
+ * Registra un nuevo usuario
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Usuario registrado
+ */
+const registerUser = async (req, res, next) => {
+  try {
+    const user = req.body;
+    const newUser = await authService.register(user);
+
+    responseHandler(res, 201, "User created successfully", newUser);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.EMAIL_ALREADY_EXISTS:
+        next(createHttpError(409, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Autentica un usuario
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Token de sesión
+ */
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const token = await authService.authUser(email, password);
+
+    responseHandler(res, 200, "User logged in successfully", token);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.INVALID_CREDENTIALS:
+        next(createHttpError(400, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
