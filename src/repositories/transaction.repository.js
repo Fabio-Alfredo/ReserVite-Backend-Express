728735b13803ece81f2cf92c11d4a2b1b3a5914b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-04 22:27:46 -0600

    refactor(transaction.repository): add documentation for functions :sparkles:

--- src/repositories/transaction.repository.js
+++ src/repositories/transaction.repository.js
@@ -1,14 +1,32 @@
 const { sequelize } = require("../domain/models");
 
+/**
+ * Repositorio de transacciones
+ */
+
 class TransactionRepository {
+  /**
+   * Inicia una transacción
+   * @returns {Promise<*>} - Transacción de la base de datos
+   */
   static async starTransaction() {
     return await sequelize.transaction();
   }
 
+  /**
+   * Confirma una transacción
+   * @param {Object} t - Transacción de la base de datos
+   * @return {Promise<*>} - Transacción confirmada
+   */
   static async commitTransaction(t) {
     await t.commit();
   }
 
+  /**
+   * Revierte una transacción
+   * @param {Object} t - Transacción de la base de datos
+   * @return {Promise<*>} - Transacción revertida
+   */
   static async rollbackTransaction(t) {
     await t.rollback();
   }
