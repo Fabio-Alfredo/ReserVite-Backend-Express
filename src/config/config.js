18d13e80850689def682fb9cfdd206c9fcfed323
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-05 15:49:13 -0600

    refactor(config.config): add enviroment for create recovery token :sparkles:

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,10 @@ const {
   DB_DIALECT,
   NODE_ENV,
   SECRET_KEY_JWT,
+  COMPANY_NAME,
+  COMPANY_EMAIL,
+  COMPANY_PASSWORD_EMAIL,
+  SECRET_KEY_RECOVERY_JWT,
 } = process.env;
 
 const validatorEnv = (env, name) => {
@@ -21,12 +25,24 @@ const config = {
   development: {
     port: validatorEnv(PORT, 'PORT'),
     secret_key_jwt: validatorEnv(SECRET_KEY_JWT, 'SECRET_KEY_JWT'),
+    secret_key_recovery_jwt: validatorEnv(
+      SECRET_KEY_RECOVERY,
+      'SECRET_KEY_RECOVERY'
+    ),
     db: {
       database: validatorEnv(DB_DATABASE_NAME, 'DB_DATABASE_NAME'),
       username: validatorEnv(DB_USER_NAME, 'DB_USER_NAME'),
       password: validatorEnv(DB_PASSWORD, 'DB_PASSWORD'),
       host: validatorEnv(DB_HOST, 'DB_HOST'),
       dialect: validatorEnv(DB_DIALECT, 'DB_DIALECT'),
+    },
+    email: {
+      company_name: validatorEnv(COMPANY_NAME, 'COMPANY_NAME'),
+      company_email: validatorEnv(COMPANY_EMAIL, 'COMPANY_EMAIL'),
+      company_password_email: validatorEnv(
+        COMPANY_PASSWORD_EMAIL,
+        'COMPANY_PASSWORD_EMAIL'
+      ),
     },
   },
   production: {
