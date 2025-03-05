87b7f1634bc9cd837dc5819606362956e7a9ed89
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-05 16:40:52 -0600

    refactor(auth.controller): add controller for recover password :sparkles:

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,7 @@
-const createHttpError = require('http-errors');
-const ErrorCodes = require('../utils/errors/error.codes');
-const authService = require('../services/auth.service');
-const responseHandler = require('../helpers/responsehandler.helper');
+const createHttpError = require("http-errors");
+const ErrorCodes = require("../utils/errors/error.codes");
+const authService = require("../services/auth.service");
+const responseHandler = require("../helpers/responsehandler.helper");
 
 /**
  * Registra un nuevo usuario
@@ -16,7 +16,7 @@ const registerUser = async (req, res, next) => {
     const user = req.body;
     const newUser = await authService.register(user);
 
-    responseHandler(res, 201, 'User created successfully', newUser);
+    responseHandler(res, 201, "User created successfully", newUser);
   } catch (e) {
     switch (e.code) {
       case ErrorCodes.USER.EMAIL_ALREADY_EXISTS:
@@ -44,7 +44,7 @@ const loginUser = async (req, res, next) => {
     const { email, password } = req.body;
     const token = await authService.authUser(email, password);
 
-    responseHandler(res, 200, 'User logged in successfully', token);
+    responseHandler(res, 200, "User logged in successfully", token);
   } catch (e) {
     switch (e.code) {
       case ErrorCodes.USER.INVALID_CREDENTIALS:
@@ -59,7 +59,36 @@ const loginUser = async (req, res, next) => {
   }
 };
 
+/**
+ * Recupera la contraseña de un usuario
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Token de recuperación
+ */
+const recoveryPassword = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const token = await authService.recoverPassword(email, name);
+
+    responseHandler(res, 200, "Recovery token sent successfully", token);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  recoveryPassword,
 };
