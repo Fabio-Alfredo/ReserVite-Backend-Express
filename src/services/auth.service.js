1423bfb65a9f99529429a46942f9528a7fb45608
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-05 16:00:35 -0600

    refactor(user.model): add new colum in table user :sparkles:

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,9 @@
-const user_repository = require('../repositories/user.repository');
-const ServiceError = require('../utils/errors/service.error');
-const ErrorCodes = require('../utils/errors/error.codes');
-const createStrategy = require('../utils/jwt/jwt.util');
-const Transactions = require('../repositories/transaction.repository');
+const user_repository = require("../repositories/user.repository");
+const ServiceError = require("../utils/errors/service.error");
+const ErrorCodes = require("../utils/errors/error.codes");
+const createStrategy = require("../utils/jwt/jwt.util");
+const Transactions = require("../repositories/transaction.repository");
+const createEmail = require("../helpers/sedEmail.helper");
 
 /**
  * Registra un nuevo usuario
@@ -17,7 +18,7 @@ const register = async (user) => {
     const exists = await user_repository.findByEmail(user.email);
     if (exists) {
       throw new ServiceError(
-        'Email already exists',
+        "Email already exists",
         ErrorCodes.USER.EMAIL_ALREADY_EXISTS
       );
     }
@@ -28,7 +29,7 @@ const register = async (user) => {
   } catch (e) {
     await Transactions.rollbackTransaction(t);
     throw new ServiceError(
-      e.message || 'Error registering user',
+      e.message || "Error registering user",
       e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
     );
   }
@@ -49,12 +50,12 @@ const authUser = async (email, password) => {
 
     if (!user || !(await user.validPassword(password))) {
       throw new ServiceError(
-        'Invalid email or password',
+        "Invalid email or password",
         ErrorCodes.USER.INVALID_CREDENTIALS
       );
     }
 
-    const tokenStrategy = createStrategy.createTokenStrategy('JWT');
+    const tokenStrategy = createStrategy.createTokenStrategy("JWT");
 
     const tokenData = tokenStrategy.generateToken({
       id: user.id,
@@ -68,7 +69,41 @@ const authUser = async (email, password) => {
   } catch (e) {
     await Transactions.rollbackTransaction(t);
     throw new ServiceError(
-      e.message || 'Error authenticating user',
+      e.message || "Error authenticating user",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
+const recoverPassword = async (email, name) => {
+  const t = await Transactions.starTransaction();
+  try {
+    const exists = await user_repository.findByEmailAndUsername(email, name);
+    if (!exists) {
+      throw new ServiceError(
+        "Invalid email or name",
+        ErrorCodes.USER.INVALID_CREDENTIALS
+      );
+    }
+    const tokenStrategy = createStrategy.createTokenStrategy("RECOVERY_JWT");
+    const token = tokenStrategy.generateToken({
+      id: exists.id,
+      email: exists.email,
+    });
+
+    await createEmail(
+      email,
+      "Recover Password",
+      "Recover your password",
+      `<a href="http://localhost:3000/recover/${token}">Recover Password</a>`
+    );
+
+    await Transactions.commitTransaction(t);
+    return token;
+  } catch (e) {
+    await Transactions.rollbackTransaction(t);
+    throw new ServiceError(
+      e.message || "Error recovering password",
       e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
     );
   }
@@ -77,4 +112,5 @@ const authUser = async (email, password) => {
 module.exports = {
   register,
   authUser,
+  recoverPassword,
 };
