195fc4c2af150d6eebac9f3ade143df2c0d8c961
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-05 22:50:12 -0600

    add(role.repository): add new functions for roles :sparkles:

---
+++ src/repositories/role.repository.js
@@ -0,0 +1,40 @@
+const { Roles } = require("../domain/models");
+
+/**
+ * Añade un nuevo rol a la base de datos
+ *
+ * @param {Object} role - Datos del rol
+ * @param {Object} t - Transacción de la base de datos
+ * @returns {Promise<*>} - Rol creado
+ */
+const create = async (role, t) => {
+  const newRole = await Roles.create(role, { transaction: t });
+  return newRole;
+};
+
+/**
+ * Busca un rol por su id
+ *
+ * @param {string} id - Id del rol
+ * @returns {Promise<*>} - Rol encontrado
+ */
+const findById = async (id) => {
+  const role = await Roles.findByPk(id);
+  return role;
+};
+
+/**
+ * Elimina un rol por su id
+ *
+ * @param {string} id - Id del rol
+ * @param {Object} t - Transacción de la base de datos
+ */
+const deleteById = async (id, t) => {
+  await Roles.destroy({ where: { id }, transaction: t });
+};
+
+module.exports = {
+  create,
+  findById,
+  deleteById,
+};
