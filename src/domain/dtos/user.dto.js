bec84b4e84fa4bf853f83af663f3506758c548e5
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-06 22:14:22 -0600

    add(user.dto): new tdo for mapping information user :sparkles:

---
+++ src/domain/dtos/user.dto.js
@@ -0,0 +1,22 @@
+const userDTO = (user) => {
+  let roles = [];
+  
+  if (user.roles.length > 0) {
+    roles = user.roles.map((role) => {
+      return {
+        id: role.id,
+        name: role.name,
+      };
+    });
+  }
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    roles: roles,
+  };
+};
+
+
+module.exports = userDTO;
