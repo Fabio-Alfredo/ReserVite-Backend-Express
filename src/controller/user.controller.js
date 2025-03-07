9beeb64cb4d3d6229c25a0e4a28fe1f827115959
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-07 15:43:35 -0600

    add(user.controller): new controller for find users by id, by role and by email :sparkles:

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,7 @@
-const user_service = require("../services/user.service");
+const { user_service } = require("../services");
 const responseHandler = require("../helpers/responsehandler.helper");
 const createHttpError = require("http-errors");
-const {ErrorCodes} = require("../utils/errors");
+const { ErrorCodes } = require("../utils/errors");
 const userDTO = require("../domain/dtos/user.dto");
 
 /**
@@ -37,6 +37,94 @@ const assingRole = async (req, res, next) => {
   }
 };
 
+/**
+ * Busca todos los usuarios por roles
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Usuarios encontrados
+ */
+const findAllByRoles = async (req, res, next) => {
+  try {
+
+    const { roleId } = req.query ;
+    const users = await user_service.findAllByRole(roleId);
+    responseHandler(res, 200, "Users found", users || []);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.ROLE.ROLE_NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Busca un usuario por su id
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Usuario encontrado
+ */
+const findById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await user_service.findById(id);
+    responseHandler(res, 200, "User found", userDTO(user));
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Busca un usuario por su email
+ *
+ * @param {Object} req - Datos de la solicitud
+ * @param {Object} res - Respuesta de la solicitud
+ * @param {Function} next - Pasa el control al siguiente manejador de solicitudes
+ * @returns {Object} - Usuario encontrado
+ */
+const findByEmail = (req, res, next) => {
+  try {
+    const { email } = req.param;
+    const user = user_service.findByEmail(email);
+    responseHandler(res, 200, "User found", userDTO(user));
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next;
+    }
+  }
+};
+
 module.exports = {
   assingRole,
+  findAllByRoles,
+  findById,
+  findByEmail,
 };
