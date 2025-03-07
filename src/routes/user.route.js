d195569834766a513dc6d36c207810aef2344a73
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-06 22:24:56 -0600

    refactor(user.route): usage validations for update role in user :sparkles:

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,15 @@
 const Route = require("express").Router;
 const user_controller = require("../controller/user.controller");
+const validatorHandler = require("../middlewares/validator.middleware");
+const { assingRole } = require("../validators/user.validator");
 
 const userRouter = Route();
 
-userRouter.put("/assign-role", user_controller.assingRole);
-
+userRouter.put(
+  "/assign-role",
+  assingRole,
+  validatorHandler,
+  user_controller.assingRole
+);
 
 module.exports = userRouter;
