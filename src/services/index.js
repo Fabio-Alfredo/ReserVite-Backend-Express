c7c9112bbbc8ec2accd5d80d9cf2a2db5267ad5e
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-07 15:42:41 -0600

    add(index.js): new archive for export services :sparkles:

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const role_service = require("./role.service");
+const auth_service = require("./auth.service");
+const user_service = require("./user.service");
+
+module.exports = {
+  role_service,
+  auth_service,
+  user_service,
+};
