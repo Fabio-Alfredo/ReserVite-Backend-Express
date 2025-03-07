418a519d31dbaf8265c05f503c956cbefbc5e37b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-06 22:16:41 -0600

    add: routes for update roles in user :sparkles:

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,12 @@
 const Route = require("express").Router;
 const authRouter = require("./auth.route");
 const roleRouter = require("./role.route");
+const userRouter = require("./user.route");
 
 const routes = Route();
 
 routes.use("/auth", authRouter);
 routes.use("/role", roleRouter);
+routes.use("/user", userRouter);
 
 module.exports = routes;
