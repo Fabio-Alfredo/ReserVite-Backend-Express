04f324b517d9e9bc622aceb4c3006270cd36bd1b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 12:10:12 -0600

    add(event.service): new services for find events :sparkles:

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -3,6 +3,13 @@ const { user_service } = require("../services");
 const Transactions = require("../repositories/transaction.repository");
 const { ServiceError, ErrorCodes } = require("../utils/errors");
 
+/**
+ * Crea un nuevo evento
+ *
+ * @param {Object} event - Datos del evento
+ * @param {Object} organizer - Organizador del evento
+ * @returns {Promise<*>} - Evento creado
+ */
 const createEvent = async (event, organizer) => {
   const t = await Transactions.starTransaction();
   try {
@@ -19,11 +26,11 @@ const createEvent = async (event, organizer) => {
       );
     }
 
-    if(event.initial_date < new Date() || event.end_date <= event.initial_date) {
-      throw new ServiceError(
-        "Invalid dates",
-        ErrorCodes.EVENT.INVALID_DATES
-      );
+    if (
+      event.initial_date < new Date() ||
+      event.end_date <= event.initial_date
+    ) {
+      throw new ServiceError("Invalid dates", ErrorCodes.EVENT.INVALID_DATES);
     }
 
     const newEvent = await event_repository.create(event, t);
@@ -40,6 +47,96 @@ const createEvent = async (event, organizer) => {
   }
 };
 
+/**
+ * Busca un evento por su id
+ *
+ * @param {string} id - Id del evento
+ * @returns {Promise<*>} - Evento encontrado
+ */
+const findById = async (id) => {
+  try {
+    const event = await event_repository.findById(id);
+    if (!event) {
+      throw new ServiceError(
+        "Event not found",
+        ErrorCodes.EVENT.EVENT_NOT_FOUND
+      );
+    }
+    return event;
+  } catch (e) {
+    throw new ServiceError(
+      e.message || "Error finding event",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
+/**
+ * Busca todos los eventos
+ *
+ * @returns {Promise<*>} - Eventos encontrados
+ */
+const findAll = async () => {
+  try {
+    const events = await event_repository.findAll();
+
+    return events || [];
+  } catch (e) {
+    throw new ServiceError(
+      e.message || "Error finding events",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
+/**
+ * Busca todos los eventos de un organizador
+ *
+ * @param {string} organizerId - Id del organizador
+ * @returns {Promise<*>} - Eventos encontrados
+ */
+const findAllByOrganizer = async (organizerId) => {
+  try {
+    const user = await user_service.findById(organizerId);
+    if (!user || user.role !== "ORGA") {
+      throw new ServiceError("Organizer not found", ErrorCodes.USER.NOT_FOUND);
+    }
+
+    return await event_repository.findAllByOrganizer(organizer);
+  } catch (e) {
+    throw new ServiceError(
+      e.message || "Error finding events",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
+/**
+ * Busca todos los eventos por una fecha
+ *
+ * @param {string} date - Fecha del evento
+ * @returns {Promise<*>} - Eventos encontrados
+ */
+const findAllByDate = async (date) => {
+  try {
+    if (date < new Date()) {
+      throw new ServiceError("Invalid date", ErrorCodes.EVENT.INVALID_DATES);
+    }
+    const events = await event_repository.findAllByDate(date);
+
+    return events || [];
+  } catch (e) {
+    throw new ServiceError(
+      e.message || "Error finding events",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
 module.exports = {
   createEvent,
+  findById,
+  findAll,
+  findAllByOrganizer,
+  findAllByDate,
 };
