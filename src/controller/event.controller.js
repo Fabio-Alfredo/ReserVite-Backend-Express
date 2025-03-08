1fa4967b1d12e743fdf60d7a9944f9b95ba74ba6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 11:19:17 -0600

    add(event.controller): new controller for create events :sparkles:

---
+++ src/controller/event.controller.js
@@ -0,0 +1,29 @@
+const createHttpError = require("http-errors");
+const { ErrorCodes } = require("../utils/errors");
+const { event_service } = require("../services");
+const responseHandler = require("../helpers/responsehandler.helper");
+
+const createEvent = async (req, res, next) => {
+  try {
+    const event = req.body;
+    const organizer = req.user;
+    const newEvent = await event_service.createEvent(event, organizer);
+
+    responseHandler(res, 201, "Event created successfully", newEvent);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.EVENT.EVENT_ALREADY_EXISTS:
+        next(createHttpError(409, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+module.exports = {
+  createEvent,
+};
