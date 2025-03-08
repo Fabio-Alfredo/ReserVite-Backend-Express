32ef827a9d1482e5abd00f0b9af6287ed6fce93c
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-07 19:11:02 -0600

    add(user.route):usage new validations for routes :sparkles:

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,12 @@
 const Route = require("express").Router;
 const user_controller = require("../controller/user.controller");
 const validatorHandler = require("../middlewares/validator.middleware");
-const { assingRole } = require("../validators/user.validator");
+const {
+  assingRoleValidator,
+  findAllByRoleValidator,
+  findByEmailValidator,
+  findByIdValidator,
+} = require("../validators/user.validator");
 
 const userRouter = Route();
 
@@ -10,14 +15,14 @@ const userRouter = Route();
  * @description Asigna un rol a un usuario
  * @access Privado (admin)
  * @middleware
- * - assingRole => valida los campos del body
+ * - assingRoleValidator => valida los campos del body
  * - validatorHandler => maneja los errores de validacion
  * @controller
  * - user_controller.assingRole => asigna un rol a un usuario
  */
 userRouter.put(
   "/assign-role",
-  assingRole,
+  assingRoleValidator,
   validatorHandler,
   user_controller.assingRole
 );
@@ -26,25 +31,49 @@ userRouter.put(
  * @route GET /user/all-by-role
  * @description Busca todos los usuarios por roles
  * @access Privado (admin)
+ * @middleware
+ * - findAllByRoleValidator => valida los campos del query
+ * - validatorHandler => maneja los errores
  * @controller
  * - user_controller.findAllByRoles => busca todos los usuarios por roles
  */
-userRouter.get("/all-by-role/", user_controller.findAllByRoles);
+userRouter.get(
+  "/all-by-role/",
+  findAllByRoleValidator,
+  validatorHandler,
+  user_controller.findAllByRoles
+);
 /**
  * @route GET /user/find-by-id/:id
  * @description Busca un usuario por su id
  * @access Privado (admin)
+ * @middleware
+ * - findByIdValidator => valida los campos del param
+ * - validatorHandler => maneja los
  * @controller
  * - user_controller.findById => busca un usuario por su id
  */
-userRouter.get("/find-by-id/:id", user_controller.findById);
+userRouter.get(
+  "/find-by-id/:id",
+  findByIdValidator,
+  validatorHandler,
+  user_controller.findById
+);
 /**
  * @route GET /user/find-by-email/:email
  * @description Busca un usuario por su email
  * @access Privado (admin)
+ * @middleware
+ * - findByEmailValidator => valida los campos del param
+ * - validatorHandler => maneja los errores
  * @controller
  * - user_controller.findByEmail => busca un usuario por su email
  */
-userRouter.get("/find-by-email/:email", user_controller.findByEmail);
+userRouter.get(
+  "/find-by-email/:email",
+  findByEmailValidator,
+  validatorHandler,
+  user_controller.findByEmail
+);
 
 module.exports = userRouter;
