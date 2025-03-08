599b72a0b4799c449f72c023879ad70183a15dc6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 11:20:23 -0600

    add(index): include route for events :sparkles

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,11 +2,13 @@ const Route = require("express").Router;
 const authRouter = require("./auth.route");
 const roleRouter = require("./role.route");
 const userRouter = require("./user.route");
+const eventRouter = require("./event.route");
 
 const routes = Route();
 
 routes.use("/auth", authRouter);
 routes.use("/role", roleRouter);
 routes.use("/user", userRouter);
+routes.use("/event", eventRouter);
 
 module.exports = routes;
