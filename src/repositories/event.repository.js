486206796164a06df43cab7859f3cbf390b0cf61
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 12:09:38 -0600

    add(event.service): new method for events :sparkles:

--- src/repositories/event.repository.js
+++ src/repositories/event.repository.js
@@ -34,6 +34,14 @@ const findAll = async () => {
   return events;
 };
 
+/**
+ * Busca un evento por su fecha de inicio, fecha de fin y ubicación
+ *
+ * @param {string} initial_date - Fecha de inicio del evento
+ * @param {string} end_date - Fecha de fin del evento
+ * @param {string} location - Ubicación del evento
+ * @returns {Promise<*>} - Evento encontrado
+ */
 const existEvent = async (initial_date, end_date, location) => {
   const event = await Events.findOne({
     where: {
@@ -56,6 +64,26 @@ const existEvent = async (initial_date, end_date, location) => {
 };
 
 /**
+ * Busca todos los eventos por una fecha
+ *
+ * @param {string} date - Fecha del evento
+ * @returns {Promise<*>} - Eventos encontrados
+ */
+const findAllByDate = async (date) => {
+  const events = await Events.findAll({
+    where: {
+      initial_date: {
+        [Op.lte]: date,
+      },
+      end_date: {
+        [Op.gte]: date,
+      },
+    },
+  });
+  return events;
+};
+
+/**
  * Busca un evento por su título
  *
  * @param {string} title - Título del evento
@@ -74,8 +102,8 @@ const findByTitle = async (title) => {
  * @param {string} organizer - Id del organizador
  * @returns {Promise<*>} - Evento encontrado
  */
-const findByOrganizer = async (organizer) => {
-  const event = await Events.findOne({
+const findAllByOrganizer = async (organizer) => {
+  const event = await Events.findAll({
     where: { organizer_id: organizer },
   });
   return event;
@@ -126,9 +154,10 @@ module.exports = {
   findById,
   findAll,
   findByTitle,
-  findByOrganizer,
+  findAllByOrganizer,
   save,
   update,
   remove,
   existEvent,
+  findAllByDate,
 };
