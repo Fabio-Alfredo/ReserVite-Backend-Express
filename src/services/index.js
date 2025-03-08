ab6259d5e998932be2bc539edcc4b0883c332088
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 11:17:13 -0600

    add(index): add service for envents :sparkles:

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const role_service = require("./role.service");
 const auth_service = require("./auth.service");
 const user_service = require("./user.service");
+const event_service = require("./event.service");
 
 module.exports = {
   role_service,
   auth_service,
   user_service,
+  event_service,
 };
