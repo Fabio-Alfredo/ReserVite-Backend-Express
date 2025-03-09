b556873118dadec7f20cffaf3d53a373d5f51880
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 21:33:37 -0600

    add(event.validator): new validation for routes events :sparkles:

---
+++ src/validators/event.validator.js
@@ -0,0 +1,81 @@
+const { body } = require("express-validator");
+
+const createValidator = [
+  body("title")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage("Title event is required")
+    .isLength({ min: 5 })
+    .withMessage("Title must be at least 5 characters")
+    .bail(),
+
+  body("description")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage("Description is required")
+    .isLength({ min: 5 })
+    .withMessage("Description must be at least 5 characters")
+    .bail(),
+
+  body("initial_date")
+    .exists()
+    .notEmpty()
+    .isISO8601()
+    .withMessage("Initial date is required")
+    .bail(),
+
+  body("end_date")
+    .exists()
+    .notEmpty()
+    .isISO8601()
+    .withMessage("End date is required")
+    .bail(),
+
+  body("location")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage("Location is required")
+    .isLength({ min: 5 })
+    .withMessage("Location must be at least 5 characters")
+    .bail(),
+
+  body("capacity")
+    .notEmpty()
+    .isInt()
+    .withMessage("Capacity is required")
+    .bail(),
+
+  body("available_seats")
+    .notEmpty()
+    .isInt()
+    .withMessage("Available seats is required")
+    .bail(),
+
+  body("price")
+    .notEmpty()
+    .isFloat({ decimal_digits: "0,2" })
+    .withMessage("Price is required")
+    .bail(),
+];
+
+const idValidator = [
+  body("id").trim().notEmpty().isUUID().withMessage("Id is required").bail(),
+];
+
+const dateValidator = [
+  body("date")
+    .trim()
+    .notEmpty()
+    .isISO8601()
+    .withMessage("Date is required")
+    .bail(),
+];
+
+module.exports = {
+  createValidator,
+  idValidator,
+  dateValidator,
+};
