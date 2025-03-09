f32f25aa96e15bc22347f33f5d5e481fb0e35d72
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-08 21:19:56 -0600

    add(event.controller): new controller for find events by diferents fields :sparkles:

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -35,6 +35,124 @@ const createEvent = async (req, res, next) => {
   }
 };
 
+/**
+ * Busca un evento por su id
+ *
+ * @param {Object} req - Request
+ * @param {Object} res - Response
+ * @param {Function} next - Next function
+ * @returns {Promise<void>}
+ */
+const findById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const event = await event_service.findById(id);
+
+    responseHandler(res, 200, "Event found", event);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.EVENT.EVENT_NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Busca todos los eventos
+ *
+ * @param {Object} req - Request
+ * @param {Object} res - Response
+ * @param {Function} next - Next function
+ * @returns {Promise<void>}
+ */
+const findAll = async (req, res, next) => {
+  try {
+    const events = await event_service.findAll();
+
+    responseHandler(res, 200, "Events found", events);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Busca todos los eventos de un organizador
+ *
+ * @param {Object} req - Request
+ * @param {Object} res - Response
+ * @param {Function} next - Next function
+ * @returns {Promise<void>}
+ */
+const findAllByOrganizer = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const events = await event_service.findAllByOrganizer(id);
+
+    responseHandler(res, 200, "Events found", events);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.EVENT.EVENT_NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
+/**
+ * Busca todos los eventos por una fecha
+ *
+ * @param {Object} req - Request
+ * @param {Object} res - Response
+ * @param {Function} next - Next function
+ * @returns {Promise<void>}
+ */
+const findAllByDate = async (req, res, next) => {
+  try {
+    const { date } = req.params;
+    const events = await event_service.findAllByDate(date);
+
+    responseHandler(res, 200, "Events found", events);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.EVENT.EVENT_NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      case ErrorCodes.EVENT.INVALID_DATES:
+        next(createHttpError(400, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
+
 module.exports = {
   createEvent,
+  findById,
+  findAll,
+  findAllByOrganizer,
+  findAllByDate,
 };
