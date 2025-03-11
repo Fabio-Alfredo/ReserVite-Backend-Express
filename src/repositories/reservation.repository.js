255d2612948d72cd5f150e7fb008b87ca20f2bca
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-10 22:38:38 -0600

    add(reservation.repository): new methods for find reservatinos :sparkles:

--- src/repositories/reservation.repository.js
+++ src/repositories/reservation.repository.js
@@ -1,5 +1,12 @@
 const { Reservations } = require("../domain/models");
 
+/**
+ * Crea una nueva reserva
+ *
+ * @param {Object} reservation - Datos de la reserva
+ * @param {Object} t - Transacción de la base de datos
+ * @returns {Promise<*>} - Reserva creada
+ */
 const create = async (reservation, t) => {
   const newReservation = await Reservations.create(reservation, {
     transaction: t,
@@ -7,6 +14,29 @@ const create = async (reservation, t) => {
   return newReservation;
 };
 
+/**
+ * Busca una reserva por su id
+ *
+ * @param {string} id - Id de la reserva
+ * @returns {Promise<*>} - Reserva encontrada
+ */
+const findById = async (id) => {
+  const reservation = await Reservations.findByPk(id);
+  return reservation;
+};
+
+/**
+ * Busca todas las reservas
+ *
+ * @returns {Promise<*>} - Reservas encontradas
+ */
+const findAll = async () => {
+  const reservations = await Reservations.findAll();
+  return reservations;
+};
+
 module.exports = {
   create,
+  findById,
+  findAll,
 };
