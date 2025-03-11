eb6b8c6d68020f22fe17488474a89a30d8a7a98a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-10 22:57:34 -0600

    add(reservation.route): new routes for reservations :sparkles:

---
+++ src/routes/reservation.route.js
@@ -0,0 +1,68 @@
+const Route = require("express").Router;
+const reservation_controller = require("../controller/reservation.controller");
+const validatorHandler = require("../middlewares/validator.middleware");
+const auth_middleware = require("../middlewares/auth.middleware");
+
+const reservationRouter = Route();
+
+/**
+ * @route POST /reservation/create
+ * @description Crea una nueva reserva
+ * @access Privado
+ * @middleware
+ * - authValidator => valida el token del usuario
+ * @cotroller
+ * - reservation_controller.createReservation => crea una nueva reserva
+ */
+reservationRouter.post(
+  "/create",
+  auth_middleware.authValidator,
+  reservation_controller.createReservation
+);
+
+/**
+ * @route GET /reservation/find-by-id/:id
+ * @description Busca una reserva por su id
+ * @access Privado
+ * @middleware
+ * - authValidator => valida el token del usuario
+ * @cotroller
+ * - reservation_controller.findReservationById => busca una reserva por su id
+ */
+reservationRouter.get(
+  "/find-by-id/:id",
+  auth_middleware.authValidator,
+  reservation_controller.findReservationById
+);
+
+/**
+ * @route GET /reservation/find-all
+ * @description Busca todas las reservas
+ * @access Privado
+ * @middleware
+ * - authValidator => valida el token del usuario
+ * @cotroller
+ * - reservation_controller.findAllReservations => busca todas las reservas
+ */
+reservationRouter.get(
+  "/find-all",
+  auth_middleware.authValidator,
+  reservation_controller.findAllReservations
+);
+
+/**
+ * @route GET /reservation/find-my-reservations
+ * @description Busca todas las reservas de un usuario
+ * @access Privado
+ * @middleware
+ * - authValidator => valida el token del usuario
+ * @cotroller
+ * - reservation_controller.MyReservations => busca todas las reservas de un usuario
+ */
+reservationRouter.get(
+  "/find-my-reservations",
+  auth_middleware.authValidator,
+  reservation_controller.MyReservations
+);
+
+module.exports = reservationRouter;
