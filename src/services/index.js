bd031da9ed6a6d733523bf34ab21ccc8ca0c16aa
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-11 11:58:51 -0600

    add(index): exportation of the services for reservations :sparkles:

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const role_service = require("./role.service");
 const auth_service = require("./auth.service");
 const user_service = require("./user.service");
 const event_service = require("./event.service");
+const reservation_service = require("./reservation.service");
 
 module.exports = {
   role_service,
   auth_service,
   user_service,
   event_service,
+  reservation_service,
 };
