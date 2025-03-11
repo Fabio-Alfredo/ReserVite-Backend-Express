8af8ed717490f89e5ae13eb2a48fff3411e22880
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-11 12:00:20 -0600

    delete(reservation.validator): remove validation unnecessary :fire:

--- src/validators/reservation.validator.js
+++ src/validators/reservation.validator.js
@@ -1,23 +1,11 @@
 const { body, param } = require("express-validator");
 const status_reservation = require("../utils/constants/statusReservation.util");
-const listStatus = Object.values(status_reservation);
 
 const createValidator = [
   body("quantity")
     .notEmpty()
     .isInt()
     .withMessage("Quantity is required")
-    .bail(),
-
-  body("status")
-    .trim()
-    .notEmpty()
-    .isIn(listStatus)
-    .isString()
-    .withMessage(
-      "Status is required and must be one of the following values: " +
-        listStatus
-    )
     .bail(),
 
   body("eventId")
