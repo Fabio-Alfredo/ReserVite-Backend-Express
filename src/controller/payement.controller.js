51cb79729ba3c9740553bc096bf70a2353cbbc46
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-13 11:43:55 -0600

    add(payment.controller): new controller for create payment :sparkles:

---
+++ src/controller/payement.controller.js
@@ -0,0 +1,34 @@
+const createHttpError = require("http-errors");
+const { payment_service } = require("../services");
+const { ErrorCodes } = require("../utils/errors");
+const responseHandler = require("../helpers/responsehandler.helper");
+
+const createPayment = async (req, res, next) => {
+  try {
+    const payment = req.body;
+    const user = req.user;
+    const newPayment = await payment_service.createPayment(payment, user);
+    responseHandler(res, 201, newPayment);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.RESERVATION.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.RESERVATION.INVALID_STATUS:
+        next(createHttpError(400, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      default:
+        next(createHttpError(500, e.message));
+    }
+  }
+};
+
+module.exports = {
+  createPayment,
+};
