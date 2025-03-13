7884710711e10c2b368d218f6a396012c2caa797
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-13 11:45:58 -0600

    add: importation payment service :sparkles:

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@ const auth_service = require("./auth.service");
 const user_service = require("./user.service");
 const event_service = require("./event.service");
 const reservation_service = require("./reservation.service");
+const payment_service = require("./payment.service");
 
 module.exports = {
   role_service,
@@ -10,4 +11,5 @@ module.exports = {
   user_service,
   event_service,
   reservation_service,
+  payment_service,
 };
