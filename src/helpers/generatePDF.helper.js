149f7c47b114ba00fa6faaf48a2fb09f50134851
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-13 16:45:43 -0600

    add(generatePDF): new method for generate pdf by confirmation pay :sparkles:

---
+++ src/helpers/generatePDF.helper.js
@@ -0,0 +1,35 @@
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+
+const generateTicketPDF = (ticketData, outputPath) => {
+  return new Promise((resolve, reject) => {
+    const doc = new PDFDocument();
+    const stream = fs.createWriteStream(outputPath);
+
+    doc.pipe(stream);
+
+    doc.fontSize(18).text("🎟 Entrada de Evento", { align: "center" });
+    doc.moveDown();
+    doc.fontSize(12).text(`Nombre: ${ticketData.user}`);
+    doc.text(`Reservation id: ${ticketData.reservation}`);
+    doc.text(`Fecha de pago: ${ticketData.date}`);
+    doc.text(`Id de pago: ${ticketData.payment}`);
+    doc.text(`Ubicación: ${ticketData.location}`);
+    doc.text(`Asientos: ${ticketData.seats}`);
+    doc.moveDown();
+
+    doc.end();
+
+    stream.on("finish", () => {
+      console.log(`PDF generado correctamente en: ${outputPath}`);
+      resolve(outputPath);
+    });
+
+    stream.on("error", (err) => {
+      console.error("Error al generar el PDF:", err);
+      reject(err);
+    });
+  });
+};
+
+module.exports = generateTicketPDF;
