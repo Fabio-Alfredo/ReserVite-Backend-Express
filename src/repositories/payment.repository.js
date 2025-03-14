4e80e881ec7d93e45d46a799cfb731113a68601d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-14 17:25:41 -0600

    add(payment.repository): new method for find payment by user :sparkles:

--- src/repositories/payment.repository.js
+++ src/repositories/payment.repository.js
@@ -1,10 +1,25 @@
+const { where } = require("sequelize");
 const { Payments } = require("../domain/models");
 
+/**
+ * Crea un nuevo pago
+ *
+ * @param {Object} paymet - Datos del pago
+ * @returns {Promise<*>} - Pago creado
+ */
 const create = async (paymet) => {
   const newPayment = await Payments.create(paymet);
   return newPayment;
 };
 
+const findAllByUser = async (userId) => {
+  const payments = await Payments.findAll({
+    where: { user_id: userId },
+  });
+  return payments;
+};
+
 module.exports = {
   create,
+  findAllByUser,
 };
