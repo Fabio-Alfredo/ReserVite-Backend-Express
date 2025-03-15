fb7d93b381762657258ce003e193eec6913be9b1
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-15 17:49:10 -0600

    add(payment.controller): new controller for payments :sparkles:

--- src/controller/payement.controller.js
+++ src/controller/payement.controller.js
@@ -29,6 +29,34 @@ const createPayment = async (req, res, next) => {
   }
 };
 
+const findAllByUser = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const payments = await payment_service.findAllByUser(userId);
+    responseHandler(res, 200, payments);
+  } catch (e) {
+    switch (e.code) {
+      case ErrorCodes.USER.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.PAYMENT.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+        next(createHttpError(500, e.message));
+        break;
+      case ErrorCodes.RESERVATION.NOT_PENDING:
+        next(createHttpError(400, e.message));
+        break;
+      case ErrorCodes.RESERVATION.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      default:
+        next(createHttpError(500, e.message));
+    }
+  }
+};
 module.exports = {
   createPayment,
+  findAllByUser,
 };
