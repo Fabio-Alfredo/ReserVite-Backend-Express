5513442c70d16123969f26a11f2bed300b22149b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-15 22:00:49 -0600

    refactor(payment.service):separation of responsibilities :sparkles:

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -22,7 +22,10 @@ const createPayment = async (payment, user) => {
       payment.reservation_id
     );
 
-    if (reservation.status !== status_reservation.PENDING || reservation.userId !== user.id) {
+    if (
+      reservation.status !== status_reservation.PENDING ||
+      reservation.userId !== user.id
+    ) {
       throw new ServiceError(
         "Reservation not Found Invalid data user or status",
         ErrorCodes.RESERVATION.NOT_FOUND
@@ -31,24 +34,16 @@ const createPayment = async (payment, user) => {
 
     payment.amount = reservation.price;
     const newPayment = await payment_repository.create(payment, t);
-    await reservation_service.updateStatus(reservation.id, status_reservation.PAID, t);
+    await reservation_service.updateStatus(
+      reservation.id,
+      status_reservation.PAID,
+      t
+    );
 
     await newPayment.setUser(Existsuser.id, { transaction: t });
     await newPayment.setReservation(reservation.id, { transaction: t });
+    await sendConfirmation(reservation, Existsuser, newPayment);
 
-    const pdf = await generatePDF(
-      {
-        user: Existsuser.name,
-        reservation: reservation.id,
-        payment: newPayment.id,
-        date: new Date().toLocaleDateString(),
-        // location: reservation.event.location,
-        seats: reservation.quantity,
-      },
-      `./tickets/ticket_${newPayment.id}.pdf`
-    );
-
-    await sendEmail(Existsuser.email, "paymentConfirmation", pdf);
     await Transactions.commitTransaction(t);
 
     return newPayment;
@@ -59,6 +54,30 @@ const createPayment = async (payment, user) => {
       e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
     );
   }
+};
+
+/**
+ * Envia un correo de confirmación de la reserva
+ *
+ * @param {Object} reservation - Datos de la reserva
+ * @param {Object} user - Usuario que realiza la reserva
+ * @param {Object} payment - Datos del pago
+ * @returns {Promise<void>} - Promesa vacía
+ */
+const sendConfirmation = async (reservation, user, payment) => {
+  const pdf = await generatePDF(
+    {
+      user: user.name,
+      reservation: reservation.id,
+      payment: payment.id,
+      date: new Date().toLocaleDateString(),
+      location: reservation.event.location,
+      seats: reservation.quantity,
+    },
+    `./tickets/ticket_${payment.id}.pdf`
+  );
+
+  await sendEmail(user.email, "reservationConfirmation", pdf);
 };
 
 /**
