734803977ff789dfa5db3e40c37c576b3b83a26a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-15 18:58:33 -0600

    update(statusReservation.util): new status for reservations :sparkles

--- src/utils/constants/statusReservation.util.js
+++ src/utils/constants/statusReservation.util.js
@@ -1,7 +1,10 @@
+const userDTO = require("../../domain/dtos/user.dto");
+
 const statusReservation = {
-    PENDING: 'PENDING',
-    CONFIRMED: 'CONFIRMED',
-    CANCELED: 'CANCELED',
-}
+  PENDING: "PENDING",
+  PAID: "PAID",
+  USED: "USED",
+  CANCELED: "CANCELED",
+};
 
 module.exports = statusReservation;
