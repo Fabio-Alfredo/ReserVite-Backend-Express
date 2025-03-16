7f5ad8dbb0f69328766fdf20a5552ecf7bbf0fa6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-15 19:05:45 -0600

    add(payment.route): usaged validations in route :sparkles:

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -2,12 +2,15 @@ const Route = require("express").Router;
 const payment_controller = require("../controller/payement.controller");
 const validatorHandler = require("../middlewares/validator.middleware");
 const auth_middleware = require("../middlewares/auth.middleware");
+const { createValidator } = require("../validators/payment.validator");
 
 const paymentRouter = Route();
 
 paymentRouter.post(
   "/create",
   auth_middleware.authValidator,
+  createValidator,
+  validatorHandler,
   payment_controller.createPayment
 );
 
