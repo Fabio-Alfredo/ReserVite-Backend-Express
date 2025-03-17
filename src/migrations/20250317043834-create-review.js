0c6cca1536dd7d62d6a06898d37e8840a0d32e96
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-16 22:42:34 -0600

    add: new migration for create table review :sparkles:

---
+++ src/migrations/20250317043834-create-review.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("reviews", {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      eventId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "events",
+          key: "id",
+        },
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("reviews");
+  },
+};
