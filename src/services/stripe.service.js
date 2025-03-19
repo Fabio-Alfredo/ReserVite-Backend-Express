ac73486cad71e76c8ac170966344d062b98544cc
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-18 22:52:00 -0600

    add(stripe.service): new method for create one payment :sparkles:

---
+++ src/services/stripe.service.js
@@ -0,0 +1,25 @@
+const stripe = require("../config/stripe/stripe.config");
+const { ErrorCodes, ServiceError } = require("../utils/errors");
+
+const createCustomer = async (user, tokenId, reservation) => {
+  try {
+
+    const stripeCharge = await stripe.charges.create({
+      amount: parseFloat(reservation.price) * 100,
+      currency: "usd",
+      source: tokenId,
+      description: `Charge for ${user.email}`,
+    });
+
+    return stripeCharge;
+  } catch (e) {
+    throw new ServiceError(
+      e.message || "Error creating customer",
+      e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+    );
+  }
+};
+
+module.exports = {
+  createCustomer,
+};
