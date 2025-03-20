b2a7843a2eb13e6d3315c3b665fbf4f599600587
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-19 19:10:57 -0600

    add(payment.validator): new validation for token by payment user :sparkles:

--- src/validators/payment.validator.js
+++ src/validators/payment.validator.js
@@ -1,9 +1,13 @@
 const { body } = require("express-validator");
+const payment_methods = require("../utils/constants/paymentMethods");
+
+const paymentMethodValues = Object.values(payment_methods);
 
 const createValidator = [
   body("payment_method")
     .trim()
     .notEmpty()
+    .isIn(paymentMethodValues)
     .isString()
     .withMessage("payment_method is required")
     .bail(),
@@ -13,6 +17,13 @@ const createValidator = [
     .notEmpty()
     .isUUID()
     .withMessage("reservation_id is required")
+    .bail(),
+
+  body("tokenId")
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage("tokenId is required")
     .bail(),
 ];
 
