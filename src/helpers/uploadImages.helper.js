d66e3f99a9f749a07bc7c8c7ef518ae6e909b9ad
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-12 13:33:05 -0600

    refactor(uploadImages.helper): refactor function for generalizate usage :sparkles:

--- src/helpers/uploadImages.helper.js
+++ src/helpers/uploadImages.helper.js
@@ -8,21 +8,18 @@ const bucket = admin.storage().bucket();
  * @param {string} eventId - ID del evento
  * @returns {Promise<string>} - URL firmada de la imagen
  */
-const uploadImages = async (req, path) => {
-  if (!req.files || !req.files.image) {
-    throw new Error("No files were uploaded");
-  }
-
-  const image = req.files.image; // `image` es el campo del formulario de subida
-
-  const remotePath = `${path}/${image.name}${Date.now()}`; // Ruta remota en Firebase Storage
+const uploadImages = async (image, path) => {
+  //   if (!req.files || !req.files.image) {
+  //     throw new Error("No files were uploaded");
+  //   }
+  //   const image = req.files.image; // `image` es el campo del formulario de subida
+  const remotePath = `${path}/${image.image.name}${Date.now()}`; // Ruta remota en Firebase Storage
 
   // Subir la imagen directamente desde la memoria
   const file = bucket.file(remotePath);
-
-  await file.save(image.data, {
+  await file.save(image.image.data, {
     metadata: {
-      contentType: image.mimetype,
+      contentType: image.image.mimetype,
     },
   });
 
