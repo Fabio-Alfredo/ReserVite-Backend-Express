63281971fdf6972097b7754b0d0d3e8c1ae42820
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-12 13:09:16 -0600

    add(app.js): usage new upload files in body :sparkles:

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const errorHandler = require('./src/middlewares/errorHandler.middleware');
-const Routes = require('./src/routes/index.route');
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./src/middlewares/errorHandler.middleware");
+const Routes = require("./src/routes/index.route");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.use(Routes);
 app.use(errorHandler);
 
